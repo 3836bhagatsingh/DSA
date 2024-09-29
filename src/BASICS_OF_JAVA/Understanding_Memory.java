7b21e6c3ab84dc0167ae6f5378b71d4cb726f5e0
3836bhagatsingh <3836bhagatsingh@gmail.com>
2024-09-29 17:39:25 +0530

    Code for Understanding memory concepts.

--- src/BASICS_OF_JAVA/Understanding_Memory.java
+++ src/BASICS_OF_JAVA/Understanding_Memory.java
@@ -1,4 +1,14 @@
 package BASICS_OF_JAVA;
 
-public class Understanding_Memory {
+public class Understanding_Memory{
+    public static void main(String[] args) {
+        int num = 10; // stored in stack
+        System.out.println(num);
+
+        int[] a = {1,2,3,4,5};//  variable 'a' is stored in stack and array elements is stored in heap
+        System.out.println(a[0]);
+
+        int[] b = a; // b also point to same object as pointed by a
+        System.out.println(b[0]);
+    }
 }
